3d57891a0742e73faa9c9fed64cdcc93c1c3fc67
ivan.dimitrov <ivan.dimitrov@digital-mobility-solutions.de>
2021-07-30 14:28:44 +0200

    Initial Commit

---
+++ Bert/BertApp.swift
@@ -0,0 +1,17 @@
+//
+//  BertApp.swift
+//  Bert
+//
+//  Created by Ivan Dimitrov on 30.07.21.
+//
+
+import SwiftUI
+
+@main
+struct BertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
